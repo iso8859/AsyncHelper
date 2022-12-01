edf1e5ead41b478aa7910c741fbda542ba5f7f06
Rémi THOMAS <remi@xtware.com>
2022-12-01 11:25:49 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static async Task<int> addAsync(int a, int b)
+        {
+            await Task.Delay(100);
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            int total = Helpers.AsyncHelper.RunSync<int>(() => addAsync(1, 2));
+            Console.WriteLine(total);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # AsyncHelper
+
+Example how to call asynchronous C# function from synchronous code.
+
+https://devblogs.microsoft.com/pfxteam/task-factory-startnew-vs-new-task-start/
+
+```C#
+int total = Helpers.AsyncHelper.RunSync<int>(() => addAsync(1, 2));
+```
